9a4ef7d463a10983e9ba86fb1bf4deb0aa20b03e
Peter Bacenet <peterbacenet@icloud.com>
2021-07-22 23:08:49 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"] ;
+function destructivelyAppendCat( Jacob ) {
+cats.push( Jacob ) 
+}
+
+function destructivelyPrependCat (Marvin) {
+    cats.unshift (Marvin)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(Broom) {
+   const moreCats = [...cats, Broom]
+   return moreCats;
+}
+
+function prependCat(Arnold) {
+    const evenmoreCats = [Arnold, ...cats ]
+    return evenmoreCats;
+ }
+
+ function removeLastCat() {
+     const lessCats = cats.slice (0,cats.length-1)
+     return lessCats;
+ }
+
+ function removeFirstCat() {
+     const evenLessCats = cats.slice(1)
+     return evenLessCats;
+ }
